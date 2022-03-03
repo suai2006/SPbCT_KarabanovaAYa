471ef863e8621b8139d69a8a255c2ba76e4365b6
Вячеслав Гаврилюк <v.gavrilyuk@fortus.pro>
2022-03-03 10:04:29 +0300

    Правки

--- LR11_12/UserClass.cpp
+++ LR11_12/UserClass.cpp
@@ -2,13 +2,18 @@
 
 UserClass::UserClass(){}
 
+// Конструктор с параметром, чтоб записать "ИмяКласса_i" 
+UserClass::UserClass(string str){
+	name = str;
+}
+
 UserClass::~UserClass(){}
 
 UserClass& UserClass::operator=(const UserClass& ob){
     return *this;
 }
 ostream& operator<<(ostream& out, const UserClass& ob) {
-	out << "UserClass";
+	out << ob.name;
 	return out;
 }
 
